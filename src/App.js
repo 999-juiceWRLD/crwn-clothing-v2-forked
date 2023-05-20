ab6c2eeaf2cb494780a0f8e04b9e8bc523b42509
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-20 22:43:19 +0300

    create component folder

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import "./categories.styles.scss";
-
+import { categories } from "./categories";
 
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Women's",
-    },
-    {
-      id: 5,
-      title: "Men's",
-    },
-    
-  ];
-
   return (
     <div className="categories-container">
       
       {categories.map((category) => (
         <div key={category.id} className="category-container">
-          <div className="background-image" />
+          <div className="background-image" 
+               style={{backgroundImage: `url(${category.imageUrl})`}}
+          />
           <div className="category-body-container">
             <h2>{category.title}</h2>
             <p>Shop Now</p>
