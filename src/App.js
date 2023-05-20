f1db8db2ef2c337d448d4dccb37a549c811228a2
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-20 22:08:16 +0300

    change App.js and add more styling

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const App = () => {
     <div className="categories-container">
       
       {categories.map((category) => (
-        <div className="category-container">
+        <div key={category.id} className="category-container">
           <div className="background-image" />
           <div className="category-body-container">
             <h2>{category.title}</h2>
