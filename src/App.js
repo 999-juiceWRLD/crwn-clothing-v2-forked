a898487a34a238e2dc8bdd5c345a9aee375c007d
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-21 16:28:11 +0300

    create new branch and have tutorial of nested routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Shop from "./shop/shop.component.jsx";
 
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
     </Routes>
   );
 }
---
+++ src/shop/shop.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function Shop() {
+    return (
+        <div className="container my-4">
+            <h1>Shopping Page</h1>
+            <h6 className="py-1">
+                This page is styled with bootstrap.
+            </h6>
+        </div>
+    );
+}
+
+export default Shop;
