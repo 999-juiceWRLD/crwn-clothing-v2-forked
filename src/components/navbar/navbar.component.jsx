dc43889626fe96ed8e66d262827ec22b02dffd76
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 02:49:46 +0300

    add navbar to App.js

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
+import { Outlet } from "react-router";
 
 function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-light bg-light">
-                <span class="navbar-brand mb-0 h1">
-                    This is Kahraman's Navbar
-                </span>
-            </nav>
+            <div className="container my-3">
+                <nav class="navbar navbar-light bg-light">
+                    <span class="navbar-brand mb-0 h1 px-3">
+                        This is Kahraman's Navbar
+                    </span>
+                </nav>
+            </div>
+            <Outlet />
         </div>
     );
 }
