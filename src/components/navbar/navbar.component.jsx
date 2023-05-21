a04abdfb0cab7e9bf24b608ee0dca716c9684945
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 02:40:14 +0300

    add navbar page

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+                <span class="navbar-brand mb-0 h1">
+                    This is Kahraman's Navbar
+                </span>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
