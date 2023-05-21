fa2bdcf4f098e4c4a287b37be74248eaee9a5129
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 02:54:03 +0300

    create <Shop /> component and add it to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/navbar.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
         <Route path="home" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap";
+
+function Shop() {
+    return (
+        <div className="container">
+            <h2>Shopping Page</h2>
+            <h6>This is another page other than home page.</h6>
+        </div>
+    );
+}
+
+export default Shop;
