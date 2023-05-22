49b42aa7970c96beb2297e146db7920b27ce5684
juice wrld <ankaraunifedaisi@gmail.com>
2023-05-22 03:14:45 +0300

    remove bootstrap

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
 import { Outlet } from "react-router";
 
 function Navbar() {
     return (
-        <div>
+        <React.Fragment>
             <div className="container my-3">
                 <nav class="navbar navbar-light bg-light">
                     <span class="navbar-brand mb-0 h1 px-3">
@@ -13,7 +12,7 @@ function Navbar() {
                 </nav>
             </div>
             <Outlet />
-        </div>
+        </React.Fragment>
     );
 }
 
